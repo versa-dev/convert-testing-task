1320641a34a5ef9db184d8b0382536c20f37c574
versa-dev <tothefuture1117@gmail.com>
2021-03-25 22:04:19 +0100

    Initial setup for test(TS, Material UI)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography, Box} from '@material-ui/core';
+
+function App() {
+  return (
+    <Box>
+      <Typography
+        color="primary"
+      >Hello</Typography>
+    </Box>
+  );
+}
+
+export default App;
